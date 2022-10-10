1b5d53aab6621e6d2485966e90ff4d046349b9a3
breakfireaus <myoungerc@hotmail.com>
2022-10-11 09:37:03 +1100

    added json and started js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// Packages and files needed for this project
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require('util');
+const generateMD = require('./scripts/generateMarkdown.js');
 
+// Creates file Asynchronously
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Questions that will await input
